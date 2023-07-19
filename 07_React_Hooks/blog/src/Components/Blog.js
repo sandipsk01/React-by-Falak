6fc3aad6acc477add9229f271820f53e0b2a55e5
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-07-19 23:43:32 +0530

    useRef in Blog

---
+++ 07_React_Hooks/blog/src/Components/Blog.js
@@ -0,0 +1,93 @@
+import { useState, useRef } from "react";
+
+//Blogging App using Hooks
+export default function Blog(){
+    
+    // const [title, setTitle]= useState("");
+    // const [content, setContent]= useState("");
+
+    const [formData, setFormData]=useState({title:"",content:""})
+    const [blogs, setBlogs]=useState([]);
+    const titleRef=useRef(null);
+    //Passing the synthetic event as argument to stop refreshing the page on submit
+    function handleSubmit(e){
+        e.preventDefault();
+
+        // setBlogs([{title,content},...blogs]);       //rest operator to keep whatever their already present in array
+        // setTitle("");
+        // setContent("");
+        setBlogs([{title:formData.title,content:formData.content},...blogs]);
+        setFormData({title:"", content:""});
+        titleRef.current.focus();
+        console.log({blogs})
+    }
+
+    function removeBlog(i){
+        setBlogs(blogs.filter((blog, index)=>i!==index))
+    }
+    return(
+        <>
+        {/* Heading of the page */}
+        <h1>Write a Blog!</h1>
+
+        {/* Division created to provide styling of section to the form */}
+        <div className="section">
+
+        {/* Form for to write the blog */}
+            <form onSubmit={handleSubmit}>
+
+                {/* Row component to create a row for first input field */}
+                <Row label="Title">
+                        <input className="input"
+                                placeholder="Enter the Title of the Blog here.."
+                                value={formData.title}
+                                ref={titleRef}
+                                onChange={(e)=>setFormData({title:e.target.value, content:formData.content})}
+                                // partial setState doesn't work in hook so mention both the keys even though they are no setting
+                                />
+                </Row >
+
+                {/* Row component to create a row for Text area field */}
+                <Row label="Content">
+                        <textarea className="input content"
+                                placeholder="Content of the Blog goes here.."
+                                value={formData.content}
+                                onChange={(e)=>setFormData({title:formData.title,content:e.target.value})}/>
+                </Row >
+
+                {/* Button to submit the blog */}            
+                <button className = "btn">ADD</button>
+            </form>
+                     
+        </div>
+
+        <hr/>
+
+        {/* Section where submitted blogs will be displayed */}
+        <h2> Blogs </h2>
+        {blogs.map((blog,i)=>(
+            <div className="blog" key={i}>
+                <h3>{blog.title}</h3>
+                <p>{blog.content}</p>
+                <div className="blog-btn">
+                    <button className="btn remove" onClick={()=>removeBlog(i)}>
+                        Delete
+                    </button>
+                </div>
+            </div>
+        ))}
+        </>
+        )
+    }
+
+//Row component to introduce a new row section in the form
+function Row(props){
+    const{label} = props;
+    return(
+        <>
+        <label>{label}<br/></label>
+        {props.children}
+        <hr />
+        </>
+    )
+}
