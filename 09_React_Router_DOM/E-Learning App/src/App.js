f3a6089c4958ce37842c960665bd15dad93e569d
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-08-07 18:35:12 +0530

    E-learning app: useOutletContext

--- 09_React_Router_DOM/E-Learning App/src/App.js
+++ 09_React_Router_DOM/E-Learning App/src/App.js
@@ -1,14 +1,43 @@
 import Hero from "./pages/app/hero/Hero";
 import Nav from "./components/nav/Nav";
 import Courses from "./pages/app/courses/Courses";
+import Details from "./pages/app/details/Details";
+import Learn from "./pages/app/learn/Learn";
+import Chapter from "./pages/app/chapter/Chapter";
+import Page404 from "./pages/misc/Page404/Page404";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const browserRouter=createBrowserRouter([
+    { 
+      path:'/', 
+      element:<Nav />,
+      children:[
+        {index:true, element:<Hero />},
+        {
+          path:'/courses', 
+          children:[
+            {index:true, element:<Courses />},
+            {path:':courseId', element:<Details />}
+          ]
+        },
+        {
+          path:'/learn/:courseId',
+          element:<Learn />,
+          children:[
+            {
+              path:'chapter/:chapterId',
+              element: <Chapter />
+            }
+          ]
+        }
+      ]
+    },
+    {errorElement: <Page404 />},
+  ]);
   return (
-    <div className="App">
-      <Nav />
-      <Hero />
-      <Courses />
-    </div>
+    <RouterProvider router={browserRouter} />
   );
 }
 
--- 09_React_Router_DOM/E-Learning App/src/components/nav/Nav.jsx
+++ 09_React_Router_DOM/E-Learning App/src/components/nav/Nav.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import style from "./Nav.module.css";
-
+import { Outlet, NavLink, Link } from "react-router-dom";
 function Nav() {
   return (
+    <>
     <nav>
       <div className={style.nav_container}>
         <div className={style.nav_title_wrapper}>
+          <Link to='/'>
           <img
             className={style.logo}
             src="https://files.codingninjas.in/pl-ninja-16706.svg"
             alt="logo"
           />
+          </Link>
           <h4>Coding Ninjas</h4>
         </div>
         <div className={style.nav_details}>
-          <button>Courses</button>
+          <button>
+            <NavLink to='/courses'>
+              {({isActive})=>(isActive ? "On Courses" : "Go to Courses")}
+            </NavLink>
+          </button>
         </div>
       </div>
     </nav>
+    <Outlet />
+  </>
   );
 }
 
