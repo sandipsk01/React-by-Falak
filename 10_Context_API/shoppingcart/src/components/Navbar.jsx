a2dcf42eeee736d2bb55c70ed3ccea5d3709dfa1
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-08-22 15:49:03 +0530

    Context API Completed

--- 10_Context_API/shoppingcart/src/components/Navbar.jsx
+++ 10_Context_API/shoppingcart/src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import styles from "../styles/Total.module.css";
+import styles from "../styles/Navbar.module.css";
 import { useValue } from "../itemContext";
 function Navbar() {
   
-  const {item, total}= useValue();
+  const {item, total, reset, toggle}= useValue();
   return (
     <div className={styles.container}>
       <h1>Total : &#x20B9; {total}</h1>
       <h1>Items: {item}</h1>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.button} onClick={toggle}>Cart</button>
+        <button className={styles.button} onClick={reset}>Reset</button>
+      </div>
     </div>
   );
 }
